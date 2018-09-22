10f097a503b76cc4f2d4714de7ed7f1ad949f14c
Shun Lung Chen <slchen@mobisocial.us>
2018-09-22 14:03:16 +0800

    Add computer choose logic

--- Roshambo/ChooseViewController.swift
+++ Roshambo/ChooseViewController.swift
@@ -15,9 +15,15 @@ class ChooseViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func computerChoose() -> Choice {
+        let rawValue = Int(arc4random() % 3)
+        return Choice(rawValue: rawValue)!
+    }
+
     @IBAction func choose(_ sender: UIButton) {
         let choice = Choice(rawValue: sender.tag)!
-        print("user choose \(choice)")
+        let computerChoice = computerChoose();
+        print("user choose \(choice), computer choose \(computerChoice)")
     }
 
 }
