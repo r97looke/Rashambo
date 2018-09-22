dea18d5cebbe948025030966ec6778ab128a5c65
Shun Lung Chen <slchen@mobisocial.us>
2018-09-22 14:46:58 +0800

    Implement present result by code and segue for paper

--- Roshambo/ChooseViewController.swift
+++ Roshambo/ChooseViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ChooseViewController: UIViewController {
 
+    var userChoice: Choice = .scissors
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,18 +21,35 @@ class ChooseViewController: UIViewController {
         let choice = Choice(rawValue: sender.tag)!
 
         switch choice {
-            case .rock:
-                showResultByCode(.rock)
-            default:
-                break;
+        case .rock:
+            userChoice = .rock
+            showResultByCode()
+
+        case .paper:
+            userChoice = .paper
+            showResultByCodeAndSegue()
+
+        default:
+            break;
         }
     }
 
-    func showResultByCode(_ userChoice: Choice) {
+    func showResultByCode() {
         let resultViewController: ResultViewController
         resultViewController = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
         resultViewController.userChoice = userChoice
         present(resultViewController, animated: true, completion: nil)
+    }
+
+    func showResultByCodeAndSegue() {
+        performSegue(withIdentifier: "showResult", sender: self)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showResult" {
+            let resultViewController = segue.destination as! ResultViewController
+            resultViewController.userChoice = userChoice
+        }
     }
 
 }
